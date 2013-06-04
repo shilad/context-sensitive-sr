aba44c205431428badd7b91ba1deb6f2cd81febe
Sam Naden <arich@macalester.edu>
2013-06-04 16:42:36 -0500

    Whitespaces

--- SRSurvey/test/unit/srsurvey/ExperimentalGroupTests.groovy
+++ SRSurvey/test/unit/srsurvey/ExperimentalGroupTests.groovy
@@ -3,17 +3,13 @@ package srsurvey
 
 
 import grails.test.mixin.*
-
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-
 @Mock([InterestToGroup, Survey, Interest])
 @TestFor(ExperimentalGroup)
 class ExperimentalGroupTests {
-
     void testExpGroup() {
-
         //Testing the variable
         ExperimentalGroup g = new ExperimentalGroup()
         g.name = "Biology"
@@ -27,18 +23,12 @@ class ExperimentalGroupTests {
         interest.setText("Computer Science")
         interest.save(flush: true)
         assertEquals(interest.text, "Computer Science")
-
         InterestToGroup interestToGroup = new InterestToGroup()
         interestToGroup.setInterest(interest)
         interestToGroup.setGroup(g)
         interestToGroup.save(flush: true)
-
         assertEquals(InterestToGroup.count(),1)
         assertEquals(Interest.count(),1)
         assertEquals(ExperimentalGroup.count(),1)
-
-
-
-
     }
 }
