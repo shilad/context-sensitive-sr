5cf079fc45f2a73d8e6967110a91022091a3177c
Zixiao Wang <zwang@macalester.edu>
2013-06-04 11:52:46 -0500

    Added new constructors for the domain model Added some unit tests

--- SRSurvey/grails-app/domain/srsurvey/Question.groovy
+++ SRSurvey/grails-app/domain/srsurvey/Question.groovy
@@ -12,4 +12,16 @@ class Question {
 
     static constraints = {
     }
+
+    Question(Double result, Integer questionNumber){
+        this.result = result
+        this.questionNumber = questionNumber
+    }
+
+    Question(Double result, Integer questionNumber, Interest interest1, Interest interest2){
+        this.result = result
+        this.questionNumber = questionNumber
+        this.interest1 = interest1
+        this.interest2 = interest2
+    }
 }
