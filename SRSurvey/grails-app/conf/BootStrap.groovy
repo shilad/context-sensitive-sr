f71afadb492d846ba11e44c3b3c14ed5e07d761b
Zixiao Wang <zwang@macalester.edu>
2013-06-05 13:27:49 -0500

    added fake person data to BootStrap

--- SRSurvey/grails-app/conf/BootStrap.groovy
+++ SRSurvey/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,40 @@
+import srsurvey.Person
+
 class BootStrap {
 
     def init = { servletContext ->
+        Person ari = new Person()
+        ari.setEmail("aweiland@macalester.edu")
+        ari.save(flush: true)
+
+        Person ben = new Person()
+        ben.setEmail("bhillman@macalester.edu")
+        ben.save(flush: true)
+
+        Person jesse = new Person()
+        jesse.setEmail("jrussel1@macalester.edu")
+        jesse.save(flush: true)
+
+        Person mat = new Person()
+        mat.setEmail("mgiesel@macalester.edu")
+        mat.save(flush: true)
+
+        Person rebecca = new Person()
+        rebecca.setEmail("rgold1@macalester.edu")
+        rebecca.save(flush: true)
+
+        Person sam = new Person()
+        sam.setEmail("snaden@macalester.edu")
+        sam.save(flush: true)
+
+        Person ken = new Person()
+        ken.setEmail("zwang@macalester.edu")
+        ken.save(flush: true)
+
+        Person yulun = new Person()
+        yulun.setEmail("yli2@macalester.edu")
+        yulun.save(flush: true)
+
     }
     def destroy = {
     }
