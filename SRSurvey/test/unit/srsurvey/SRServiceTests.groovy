18345ca7238199897810f9b686d1d21c65ac69c9
Ben Hillmann <arich@macalester.edu>
2013-06-06 15:55:00 -0500

    Changed and added all the tests

---
+++ SRSurvey/test/unit/srsurvey/SRServiceTests.groovy
@@ -0,0 +1,17 @@
+package srsurvey
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(SRService)
+class SRServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
