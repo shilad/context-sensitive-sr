595160bdb045e139a07167a712fa704f6390b3fc
Zixiao Wang <zwang@macalester.edu>
2013-06-06 17:38:04 -0500

    Working on storing user inputs from ratings page

--- SRSurvey/grails-app/controllers/srsurvey/RatingController.groovy
+++ SRSurvey/grails-app/controllers/srsurvey/RatingController.groovy
@@ -14,17 +14,25 @@ class RatingController {
 
         List<Question> questions = new SRService().getQuestions(p.group)
 
-        render(view:'ratings', model:[questions:questions])
+        render(view:'ratings', model:[questions:questions.get(0)])
     }
 
     def processForm(){
-        print(params)
         for(i in params['checks']){
-            print("add this into database for 'I don't know' checkbox")
+            //"add this into database for 'I don't know' checkbox"
+            //i is the id for interest
+            Interest interest = Interest.get(i)
+            int count = interest.unknown
+            interest.unknown++
+            interest.save(flush: true)
         }
         for(q in params){
+            print(q)
             if(q.key!="action"&&q.key!="controller"){
-                print("This is the question number "+q.key+" and this is the score "+q.value+". Put these into the database.")
+                //"This is the question id "+q.key+" and this is the score "+q.value+". Put these into the database."
+//                Question question = Question.get(Integer.parseInt(q.key))
+//                question.result = q.value
+//                question.save(flush: true)
             }
         }
     }
