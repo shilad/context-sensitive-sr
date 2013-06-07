97b606480b53dceef55dcd19f067c4157632d192
Zixiao Wang <zwang@macalester.edu>
2013-06-07 11:20:51 -0500

    Fixing the ratings page

--- SRSurvey/grails-app/controllers/srsurvey/RatingController.groovy
+++ SRSurvey/grails-app/controllers/srsurvey/RatingController.groovy
@@ -4,17 +4,19 @@ class RatingController {
 
     def ratings(){
 
-        //TODO: Get the person from the session
-
-        Person p = Person.findByEmail("zwang@macalester.edu")
-        print(p)
-
-        SRService srService = new SRService()
-        srService.assignGroup(p, new ArrayList<Interest>())
+        //TODO: Get the person from the session, delete this part! Testing only!
+        if(session.person==null){
+            Person p = Person.findByEmail("zwang@macalester.edu")
+            session.person = p
+            SRService srService = new SRService()
+            srService.assignGroup(p, new ArrayList<Interest>())
+        }
+        Person p = session.person
 
         List<Question> questions = new SRService().getQuestions(p.group)
 
-        render(view:'ratings', model:[questions:questions.get(0)])
+        //TODO: Change here to send in all questions
+        render(view:'ratings', model:[questions:questions[0..5]])
     }
 
     def processForm(){
@@ -26,17 +28,16 @@ class RatingController {
             interest.unknown++
             interest.save(flush: true)
         }
-        for(q in params){
-            print(q)
-            if(q.key[0..4]=="radio"){
+        for(qparam in params){
+            if(qparam.key[0..4]=="radio"){
                 //"This is the question id "+q.key+" and
                 // this is the score "+q.value+". Put these into the database."
-                Question question = Question.get(Integer.parseInt(q.key))
-                question.result = q.value
-                question.save(flush: true)
-                print(q.key[0..4])
-
-                //print("This is the question number "+q.key+" and this is the score "+q.value+". Put these into the database.")
+                print(qparam)
+                String id = qparam.key[6..-1]
+                print(id)
+//                Question question = Question.get(Integer.parseInt(id))
+//                question.result = qparam.value
+//                question.save(flush: true)
             }
         }
     }
