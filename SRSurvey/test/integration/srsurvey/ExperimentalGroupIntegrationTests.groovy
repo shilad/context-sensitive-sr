96826abfcd76bb7f073897c2075439fdff070f43
Ben Hillmann <arich@macalester.edu>
2013-06-07 10:26:37 -0500

    Commit for merge

--- SRSurvey/test/integration/srsurvey/ExperimentalGroupIntegrationTests.groovy
+++ SRSurvey/test/integration/srsurvey/ExperimentalGroupIntegrationTests.groovy
@@ -17,6 +17,9 @@ class ExperimentalGroupIntegrationTests {
 
     @Test
     void testRelations() {
-
+        ExperimentalGroup g = ExperimentalGroup.findAll().get(0)
+        Person p = Person.get(0)
+        g.person = p
+        g.save()
     }
 }
