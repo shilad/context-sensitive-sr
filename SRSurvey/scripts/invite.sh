5b611b24d6f042479f894affbbb654b83ec3dee6
Zixiao Wang <zwang@macalester.edu>
2013-06-07 16:40:13 -0500

    The script now supports sending email using our Email controller

--- SRSurvey/scripts/invite.sh
+++ SRSurvey/scripts/invite.sh
@@ -2,47 +2,21 @@ function urlencode() {
     python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])" $1
 }
 
-
-if ! [ -f "$1" ]; then
-    echo "usage: $0 emails_file email_template url_prefix subject" >& 2
-    exit 1
-fi
-if [ -z "$2" ]; then
-    echo "usage: $0 emails_file email_template url_prefix subject" >& 2
-    exit 1
-fi
-if [ -z "$3" ]; then
-    echo "usage: $0 emails_file email_template url_prefix subject" >& 2
-    exit 1
-fi
-if [ -z "$4" ]; then
-    echo "usage: $0 emails_file email_template url_prefix subject" >& 2
-    exit 1
-fi
-
 EMAIL_FILE=$1
-EMAIL_TEMPLATE=`urlencode $2`
-BASE_URL=`urlencode $3`
-SUBJECT=`urlencode $4`
-ADMIN_URL=$3/administrator
+APP_URL="http://localhost:8080/SRSurvey"
+SUB_RAW="Tesing From The Survey Team!"
+SUBJECT=`urlencode $SUB_RAW`
+BASE_URL_RAW="http://localhost:8080/SRSurvey/interest/consent"
+BASE_URL=`urlencode $BASE_URL_RAW`
 
 echo EMAIL_FILE=$1
-echo EMAIL_TEMPLATE=$EMAIL_TEMPLATE
-echo BASE_URL=$BASE_URL
-echo ADMIN_URL=$ADMIN_URL
-echo SUBJECT=$SUBJECT
-
-
-wget -O - "$ADMIN_URL/resetKey"
-
-echo -n "enter new auth key from log file: "
-read key
+echo APP_URL=$APP_URL
 
 while read email; do
     echo "inviting $email"
     urlEmail=`urlencode $email`
-    wget -O - "$ADMIN_URL/invite?key=$key&email=$urlEmail&template=$EMAIL_TEMPLATE&baseUrl=$BASE_URL&subject=$SUBJECT"
-    echo
+    wget -O - "$APP_URL/email/invite?email=$urlEmail&subject=$SUBJECT&baseUrl=$BASE_URL"
+    echo "Sleeping"
     echo
     sleep 60
 done <$EMAIL_FILE
