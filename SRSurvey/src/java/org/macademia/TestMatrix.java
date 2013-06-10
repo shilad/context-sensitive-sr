7fc41ab5e1cc5e2d7ebf0ad52d3cb74a2f5499ff
Margaret Giesel <arich@macalester.edu>
2013-06-10 15:12:57 -0500

    just to pull

---
+++ SRSurvey/src/java/org/macademia/TestMatrix.java
@@ -0,0 +1,11 @@
+package org.macademia;
+
+
+import edu.macalester.wpsemsim.matrix.DenseMatrix;
+
+public class TestMatrix {
+    public static void main(String args[]) {
+       System.out.print("here");
+        DenseMatrix matrix;
+    }
+}
