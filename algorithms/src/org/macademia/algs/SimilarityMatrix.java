9c31b49a50a534869d4a756a3ec106983484b432
Zixiao Wang <zwang@macalester.edu>
2013-06-11 14:15:37 -0500

    Added the matrixToCSV method

--- algorithms/src/org/macademia/algs/SimilarityMatrix.java
+++ algorithms/src/org/macademia/algs/SimilarityMatrix.java
@@ -9,14 +9,23 @@ package org.macademia.algs;
  */
 
 import edu.macalester.wpsemsim.matrix.DenseMatrix;
+import edu.macalester.wpsemsim.matrix.DenseMatrixRow;
 import edu.macalester.wpsemsim.matrix.MemoryMappedMatrix;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
 public class SimilarityMatrix extends DenseMatrix{
+    /**
+     * Create a similarity matrix based on the data in a particular file.
+     * Using the default MAX_VALUE and MAX_PAGE_SIZE
+     * @param path Path to the matrix data file
+     * @throws IOException
+     */
     public SimilarityMatrix(File path) throws IOException {
         this(path, Integer.MAX_VALUE, DEFAULT_MAX_PAGE_SIZE);
     }
@@ -31,5 +40,63 @@ public class SimilarityMatrix extends DenseMatrix{
     public SimilarityMatrix(File path, int maxOpenPages, int maxPageSize) throws IOException {
         super(path,maxOpenPages,maxPageSize);
     }
+
+    /**
+     * Output a CSV file based on the similarity matrix
+     * The headers are 0 to getNumRows - 1
+     * @param path the output path for the CSV file
+     */
+    public void matrixToCSV(String path){
+
+        try {
+            FileWriter writer = new FileWriter(path);
+
+            //Writing headers using the interest id
+//            StringBuffer header = new StringBuffer();
+//
+//            DenseMatrixRow row1 = matrix.getRow(matrix.getRowIds()[0]);
+//            for(Integer interest:row1.asMap().keySet()){
+//                header.append(interest.toString());
+//                header.append(" , ");
+//            }
+//            header.delete(header.length()-3,header.length()-1);
+//            header.append('\n');
+//            writer.append(header.toString());
+//            //System.out.print(header.toString());
+
+            //Writing headers 0 to length-1
+            StringBuffer header = new StringBuffer();
+
+            for(int i=0;i<this.getNumRows();i++){
+                header.append(i+"");
+                header.append(" , ");
+            }
+            header.delete(header.length()-3,header.length()-1);
+            header.append('\n');
+            writer.append(header.toString());
+
+            for(DenseMatrixRow row:this){
+                LinkedHashMap<Integer,Float> rowMap = row.asMap();
+
+                //Writing similarity scores
+                StringBuffer scoresOut = new StringBuffer();
+                for(Float score:rowMap.values()){
+                    scoresOut.append(score.toString());
+                    scoresOut.append(" , ");
+                }
+                scoresOut.delete(scoresOut.length()-3,scoresOut.length()-1);
+                scoresOut.append('\n');
+                writer.append(scoresOut.toString());
+            }
+
+            writer.flush();
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
