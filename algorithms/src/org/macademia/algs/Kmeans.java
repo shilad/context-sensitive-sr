3a3bb6a7cadf31418875d5f2cbaf7cf2d2c5d2fa
Ben Hillmann <arich@macalester.edu>
2013-06-11 17:18:10 -0500

    Started implemented our own K-Means algorithm

---
+++ algorithms/src/org/macademia/algs/Kmeans.java
@@ -0,0 +1,94 @@
+package org.macademia.algs;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: research
+ * Date: 6/11/13
+ * Time: 4:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Kmeans {
+
+    private static class Centroid {
+
+        private float [] data;
+        private int id;
+
+        public Centroid() {
+            return;
+        }
+
+        public Centroid(float [] data, int id) {
+            this.data = data;
+            this.id = id;
+        }
+
+        private int getId() {
+            return id;
+        }
+
+        private void setId(int id) {
+            this.id = id;
+        }
+
+        private float[] getData() {
+            return data;
+        }
+
+        private void setData(float[] data) {
+            this.data = data;
+        }
+
+        public double getDistance(float [] point) {
+            double sum = 0;
+            for (int i=0; i < point.length; i++) {
+                sum += Math.pow(data[i] - point[i], 2);
+            }
+            return Math.sqrt(sum);
+        }
+    }
+
+    private static class Point {
+        private float [] data;
+        private int id;
+        private int mCluster;
+
+        public Point() {
+            return;
+        }
+
+        private Point(float[] data, int id, int mCluster) {
+            this.data = data;
+            this.id = id;
+            this.mCluster = mCluster;
+        }
+
+        private float[] getData() {
+            return data;
+        }
+
+        private void setData(float[] data) {
+            this.data = data;
+        }
+
+        private int getId() {
+            return id;
+        }
+
+        private void setId(int id) {
+            this.id = id;
+        }
+
+        private int getmCluster() {
+            return mCluster;
+        }
+
+        private void setmCluster(int mCluster) {
+            this.mCluster = mCluster;
+        }
+    }
+
+
+}
--- algorithms/src/org/macademia/algs/Test.java
+++ algorithms/src/org/macademia/algs/Test.java
@@ -11,5 +11,6 @@ public class Test {
 
         System.out.print("");
 
+
     }
 }
