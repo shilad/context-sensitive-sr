0b86fb87f2a21b27dbd89ea392e4061dafd414cc
Ben Hillmann <arich@macalester.edu>
2013-06-11 10:04:42 -0500

    Started working with weka files

--- algorithms/src/org/macademia/algs/Test.java
+++ algorithms/src/org/macademia/algs/Test.java
@@ -4,9 +4,19 @@ import edu.macalester.wpsemsim.matrix.DenseMatrix;
 
 import java.io.File;
 import java.io.IOException;
+import static java.lang.System.*;
 
 public class Test {
     public static void main(String args[]) throws IOException {
         DenseMatrix matrix = new DenseMatrix(new File("dat/similarity.matrix"));
+
+        out.println(matrix.getRow(100).asMap());
+
+
+
+
+        out.println(matrix.getNumRows());
+
+        out.println(matrix.getRowIds());
     }
 }
