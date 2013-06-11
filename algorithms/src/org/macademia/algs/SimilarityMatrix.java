10831d2d4bb41b703f270471b9c0ec7fa3a62c35
Zixiao Wang <zwang@macalester.edu>
2013-06-11 14:00:18 -0500

    Started SimilarityMatrix class

---
+++ algorithms/src/org/macademia/algs/SimilarityMatrix.java
@@ -0,0 +1,11 @@
+package org.macademia.algs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zixiao
+ * Date: 6/11/13
+ * Time: 1:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimilarityMatrix {
+}
