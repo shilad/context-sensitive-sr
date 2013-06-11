80b472630adec5885bc8a2cda66aa7b24cd20c5f
Sam Naden <arich@macalester.edu>
2013-06-11 10:16:17 -0500

    Merge branch 'master' of https://github.com/shilad/context-sensitive-sr
    Conflicts:
    	SRSurvey/application.properties
    	SRSurvey/grails-app/controllers/srsurvey/CommentController.groovy
    	SRSurvey/grails-app/controllers/srsurvey/InterestController.groovy
    	SRSurvey/grails-app/controllers/srsurvey/MockupController.groovy
    	SRSurvey/grails-app/controllers/srsurvey/RatingController.groovy
    	SRSurvey/grails-app/domain/srsurvey/Person.groovy
    	SRSurvey/grails-app/services/srsurvey/PersonService.groovy
    	SRSurvey/grails-app/services/srsurvey/SRService.groovy
    	SRSurvey/grails-app/views/comment/comment.gsp
    	SRSurvey/grails-app/views/comment/final.gsp
    	SRSurvey/grails-app/views/interest/consent.gsp
    	SRSurvey/grails-app/views/interest/interest.gsp
    	SRSurvey/grails-app/views/mockup/comments.gsp
    	SRSurvey/grails-app/views/mockup/ratings.gsp
    	SRSurvey/grails-app/views/rating/ratings.gsp
    	SRSurvey/scripts/invite.sh
    	SRSurvey/test/integration/srsurvey/ExperimentalGroupIntegrationTests.groovy
    	SRSurvey/web-app/css/survey.css

--- algorithms/src/org/macademia/algs/Test.java
+++ algorithms/src/org/macademia/algs/Test.java
@@ -4,9 +4,29 @@ import edu.macalester.wpsemsim.matrix.DenseMatrix;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList.*;
+
+//ArrayList<Integer> list = new ArrayList<Integer>();
+
+
 
 public class Test {
     public static void main(String args[]) throws IOException {
         DenseMatrix matrix = new DenseMatrix(new File("dat/similarity.matrix"));
+        int i = matrix.getNumRows();
+        System.out.print(i);
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
 }
