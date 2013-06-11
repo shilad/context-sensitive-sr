5e6fbdd88a7580c49fcf56da8eb00db6a19b4ed0
jesse <jrussel1@macalester.edu>
2013-06-11 13:59:41 -0500

    people

---
+++ algorithms/src/org/macademia/algs/People.java
@@ -0,0 +1,11 @@
+package org.macademia.algs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jesse
+ * Date: 6/11/13
+ * Time: 1:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class People {
+}
