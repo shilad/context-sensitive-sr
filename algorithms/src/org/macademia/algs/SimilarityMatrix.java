69ad2741c4b50ef57ebe41e87b9fc2f042bf4731
Zixiao Wang <zwang@macalester.edu>
2013-06-11 14:07:53 -0500

    Created Constructors

--- algorithms/src/org/macademia/algs/SimilarityMatrix.java
+++ algorithms/src/org/macademia/algs/SimilarityMatrix.java
@@ -7,5 +7,29 @@ package org.macademia.algs;
  * Time: 1:59 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SimilarityMatrix {
+
+import edu.macalester.wpsemsim.matrix.DenseMatrix;
+import edu.macalester.wpsemsim.matrix.MemoryMappedMatrix;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SimilarityMatrix extends DenseMatrix{
+    public SimilarityMatrix(File path) throws IOException {
+        this(path, Integer.MAX_VALUE, DEFAULT_MAX_PAGE_SIZE);
+    }
+
+    /**
+     * Create a similarity matrix based on the data in a particular file.
+     * @param path Path to the matrix data file.
+     * @param maxOpenPages The maximum number of memory mapped pages that can be open at once.
+     * @param maxPageSize The maximum size of a memory mapped page.
+     * @throws IOException
+     */
+    public SimilarityMatrix(File path, int maxOpenPages, int maxPageSize) throws IOException {
+        super(path,maxOpenPages,maxPageSize);
+    }
+
 }
