889043edccb77300084d45f7b448a7c2f1e80e8e
jesse <jrussel1@macalester.edu>
2013-06-11 09:29:16 -0500

    file parser

---
+++ algorithms/src/org/macademia/algs/FileParser.java
@@ -0,0 +1,75 @@
+package org.macademia.algs;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jesse
+ * Date: 6/10/13
+ * Time: 3:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileParser {
+    public static void main(String args[]) throws IOException {
+        HashMap phrases=reader("/Users/jesse/IdeaProjects/context-sensitive-sr/algorithms/dat/phrases.tsv",1,3,"\t");
+        Set s = phrases.keySet();
+        Object[] a=s.toArray();
+
+        HashMap people=reader("/Users/jesse/IdeaProjects/context-sensitive-sr/algorithms/dat/people.txt",0,1,"\t");
+        Set s2 = people.keySet();
+        Object[] a2=s2.toArray();
+
+        HashMap peopleInterests=reader("/Users/jesse/IdeaProjects/context-sensitive-sr/algorithms/dat/people_interests.txt",0,1,"\t");
+        Set s3 = peopleInterests.keySet();
+        Object[] a3=s3.toArray();
+        ArrayList<String> len = new ArrayList<String>();
+        System.out.println(peopleInterests.get("32"));
+        for(int i = 0; i < s2.size(); i++){
+            if(peopleInterests.get(a2[i])!=null){
+                len = (ArrayList<String>) peopleInterests.get(a2[i]);
+            }
+            if(len!=null){
+                for(int j = 0; j < len.size();j++){
+                    if(people.get(a2[i])!=null&&phrases.get( len.get(j) )!=null)
+                        System.out.println( people.get(a2[i]) +"---"+i+"--"+ phrases.get( len.get(j) ) );
+                }
+            }
+
+        }
+
+
+    }
+    public static HashMap reader(String path, int keyCol, int valCol, String splitOn) throws IOException {
+
+        HashMap temp= new HashMap();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        String line = null;
+        ArrayList<String> ar;
+        while ((line = reader.readLine()) != null) {
+            String[] lineSplit=line.split(splitOn);
+            if(temp.containsKey(lineSplit[keyCol])){
+                ArrayList<String> al = (ArrayList<String>) temp.get(lineSplit[keyCol]);
+                al.add(lineSplit[valCol]);
+                temp.put(lineSplit[keyCol],al);
+            }
+            else{
+                ar = new ArrayList<String>();
+                ar.add(lineSplit[valCol]);
+                temp.put(lineSplit[keyCol],ar);
+            }
+        }
+        return temp;
+    }
+
+}
