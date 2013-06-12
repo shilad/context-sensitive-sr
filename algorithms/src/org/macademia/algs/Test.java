a15f1192d028afb2f9faacfb2811c399270cebe8
jesse <jrussel1@macalester.edu>
2013-06-12 14:38:24 -0500

    people distance is good accuracy checked

--- algorithms/src/org/macademia/algs/Test.java
+++ algorithms/src/org/macademia/algs/Test.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String args[]) throws IOException {
         ArrayList<People> people=People_Interests.makePeopleInterests("dat/people.txt","dat/phrases.tsv","dat/people_interests.txt");
-
-        float d = People_Distance.findDistance(people.get(0),people.get(1));
-        System.out.println("The average distance between "+people.get(0).getEmail()+" and "+people.get(1).getEmail()+" is "+d);
+        People p1 = people.get(0);
+        People p2 = people.get(30);
+        float d = People_Distance.findDistance(p1,p2);
+        System.out.println("The average distance between "+p1.getEmail()+" and "+p2.getEmail()+" is "+d);
 
 //        for(int i=0;i<people.size();i++){
 //            for (int j=0;j<people.get(i).getInterest().size();j++){
