cb325741d5394c3c986075825ee4958f75500c0e
Zixiao Wang <zwang@macalester.edu>
2013-06-12 10:36:11 -0500

    Created the skeleton of the class

--- algorithms/src/org/macademia/algs/Kmeans.java
+++ algorithms/src/org/macademia/algs/Kmeans.java
@@ -1,28 +1,37 @@
-package org.macademia.algs;
-
-import java.util.ArrayList;
-
-/**
- * Created with IntelliJ IDEA.
- * User: research
- * Date: 6/11/13
- * Time: 4:55 PM
+package org.macademia.algs;  import java.util.ArrayList;  /** * Created with IntelliJ IDEA. * User: research * Date: 6/11/13 * Time: 4:55 PM
  * To change this template use File | Settings | File Templates.
  */
 public class Kmeans {
 
-    private static class Centroid {
+    private Cluster[] clusters;
+    private float[][] data;
+    private Point[] centroids;
 
-        private float [] data;
+    public Kmeans(float[][] data) {
+        this.data = data;
+    }
+
+    public float[][] getData() {
+        return data;
+    }
+
+    public void setData(float[][] data) {
+        this.data = data;
+    }
+
+    private static class Cluster {
         private int id;
 
-        public Centroid() {
-            return;
+        private Point[] points;
+
+        private Cluster() {
+
         }
 
-        public Centroid(float [] data, int id) {
-            this.data = data;
+
+        private Cluster(int id, Point[] points) {
             this.id = id;
+            this.points = points;
         }
 
         private int getId() {
@@ -33,20 +42,12 @@ public class Kmeans {
             this.id = id;
         }
 
-        private float[] getData() {
-            return data;
+        private Point[] getPoints() {
+            return points;
         }
 
-        private void setData(float[] data) {
-            this.data = data;
-        }
-
-        public double getDistance(float [] point) {
-            double sum = 0;
-            for (int i=0; i < point.length; i++) {
-                sum += Math.pow(data[i] - point[i], 2);
-            }
-            return Math.sqrt(sum);
+        private void setPoints(Point[] points) {
+            this.points = points;
         }
     }
 
@@ -89,6 +90,76 @@ public class Kmeans {
             this.mCluster = mCluster;
         }
     }
+
+    /**
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    public double getDistance(Point p1, Point p2) {
+
+//        double sum = 0;
+//        for (int i=0; i < point.length; i++) {
+//            sum += Math.pow(data[i] - point[i], 2);
+//        }
+//        return Math.sqrt(sum);
+    }
+
+    /**
+     *
+     * @param data
+     * @param k
+     * @return an array of k cluster ids
+     */
+    public int[] getClusters(float[][] data, int k){
+
+    }
+
+    /**
+     * returns k points, where the value of each dimension of the points is
+     * bounded by the minimum and maximum of that dimension over all points
+     * @param data
+     * @param k
+     * @return an array of k points
+     */
+    public Point[] getKRandomPoints(float[][] data, int k){
+
+    }
+
+    /**
+     *
+     * returns the id of the center that is closest to the given point
+     * @param point
+     * @param centroids
+     * @return the id of the cluster
+     */
+    public int getBestClusterForPoint(Point point, Point[] centroids){
+
+    }
+
+    /**
+     * Computes the intra-cluster variance over all clusters
+     * @param clusters
+     * @param centroids
+     * @return
+     */
+    public double getVariance(Cluster[] clusters, Point[] centroids){
+
+    }
+
+    /**
+     * Computes the centroids of each cluster
+     * @param clusters
+     */
+    public void computeCentroids(Cluster[] clusters){
+
+    }
+
+
+
+
+
 
 
 }
