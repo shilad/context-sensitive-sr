96a4e647eef3110392d84b83abfd240e893ba7df
jesse <jrussel1@macalester.edu>
2013-06-12 10:23:53 -0500

    starting people_distance

---
+++ algorithms/src/org/macademia/algs/People_Distance.java
@@ -0,0 +1,18 @@
+package org.macademia.algs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jesse
+ * Date: 6/12/13
+ * Time: 10:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class People_Distance {
+
+    public static float findDistance(People p1, People p2){
+        float distance = 0;
+
+
+        return distance;
+    }
+}
--- algorithms/src/org/macademia/algs/Test.java
+++ algorithms/src/org/macademia/algs/Test.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String args[]) throws IOException {
-        ArrayList<People> people = new ArrayList<People>();
-        people=People_Interests.makePeopleInterests("dat/people.txt","dat/phrases.tsv","dat/people_interests.txt");
+        ArrayList<People> people=People_Interests.makePeopleInterests("dat/people.txt","dat/phrases.tsv","dat/people_interests.txt");
         for(int i=0;i<people.size();i++){
             for (int j=0;j<people.get(i).getInterest().size();j++){
                 if(people.get(i)!=null&&people.get(i).getInterest().get(j)!=null)
