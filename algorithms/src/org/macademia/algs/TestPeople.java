925c5a747c566f07af9aea64a5222105f8c18609
jesse <jrussel1@macalester.edu>
2013-06-17 09:48:49 -0500

    serialization

--- algorithms/src/org/macademia/algs/TestPeople.java
+++ algorithms/src/org/macademia/algs/TestPeople.java
@@ -1,16 +1,20 @@
 package org.macademia.algs;
 
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.*;
 
 public class TestPeople {
 
     public static void main(String args[]) throws IOException {
-        FileWriter out=null;
+        FileOutputStream file=null;
+        ObjectOutputStream out=null;
         try{
-            out = new FileWriter("dat/personMatrix.txt");
+            file = new FileOutputStream("dat/peopleMatrix.ser");
+            out = new ObjectOutputStream(file);
         }
         catch (IOException e){
             e.printStackTrace();
@@ -21,12 +25,16 @@ public class TestPeople {
         People candidate = people.get(107);                  //0 is Shilad and 107 is Danny Kaplan in list
                                                             //findPersonByEmail(people,"shoop@macalester.edu")
 
+
+        HashMap<String,SortedMap<String,Double>> allMap = new HashMap<String, SortedMap<String, Double>>();
+
         for(int i=0;i<people.size();i++){          //Cycles through each person in list using them as the target to
-            scoresForAllCandidate(people, out, people.get(i));            //compile full matrix of scores
+            allMap.put(people.get(i).getID(),scoresForAllCandidate(people, people.get(i)));            //compile full matrix of scores
         }
 
-        out.flush();
+        out.writeObject(allMap);
         out.close();
+        file.close();
 
     }
     //SINGLE PERSON TEST - Prints out the result of the target person to the candidate
@@ -68,7 +76,7 @@ public class TestPeople {
 
     //Finds scores for all candidates relative to the target person
     // - Take in full People array and file writer object along with target person
-    public static void scoresForAllCandidate(ArrayList<People> people,FileWriter out, People target){
+    public static SortedMap<String,Double> scoresForAllCandidate(ArrayList<People> people, People target){
         double a = 0;
         SortedMap<String,Double> scoreMap = new TreeMap<String, Double>();
 
@@ -77,22 +85,18 @@ public class TestPeople {
             scoreMap.put(people.get(i).getID(),a);
         }
                                                                          //Transforms scoreMap to SortedSet
-        SortedSet<Map.Entry<String, Double>> sortedset = new TreeSet<Map.Entry<String, Double>>(
-                new Comparator<Map.Entry<String, Double>>() {
-                    public int compare(Map.Entry<String, Double> e1,
-                                       Map.Entry<String, Double> e2) {
-                        return e1.getValue().compareTo(e2.getValue());
-                    }
-                });
-        sortedset.addAll(scoreMap.entrySet());
+//        SortedSet<Map.Entry<String, Double>> sortedset = new TreeSet<Map.Entry<String, Double>>(
+//                new Comparator<Map.Entry<String, Double>>() {
+//                    public int compare(Map.Entry<String, Double> e1,
+//                                       Map.Entry<String, Double> e2) {
+//                        return e1.getValue().compareTo(e2.getValue());
+//                    }
+//                });
+
+//        sortedset.addAll(scoreMap.entrySet());
 
 
-        try{              //Appends to file the sorted set -  TargetID:[CandidateID1=val1,CandidateID2=val2,...]
-            out.append(target.getID()+":"+sortedset.toString()+"\n");
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+        return scoreMap;
     }
 
 }
