77a058797126414b1dedd2f8676e02a3ce8265ad
jesse <jrussel1@macalester.edu>
2013-06-19 10:21:18 -0500

    puting interesting people into own method

--- algorithms/src/org/macademia/algs/TestAlgorithms.java
+++ algorithms/src/org/macademia/algs/TestAlgorithms.java
@@ -13,7 +13,7 @@ public class TestAlgorithms {
     //findPersonByEmail(people,"shoop@macalester.edu")
     public static void main(String args[]) throws IOException {
         ArrayList<People> people=People_Interests.makePeopleInterests("dat/people.txt","dat/phrases.tsv","dat/people_interests.txt");
-        //People_Distance.serializeVectorMap(people,"dat/peopleVectors.ser");
+        People_Distance.serializeVectorMap(people,"dat/peopleVectors.ser");
 
         if(matrix==null){
             try {
@@ -37,6 +37,9 @@ public class TestAlgorithms {
         int[] ids = {207,12031,738,11104,15495,293,890};
         String[] names = {"Political Theory","US Politics","Biochemsitry","General Biology",
                 "Computer Science","Applied Math","Psychology"};
+        findGroupsByInterestingPeople(ids,names,people);
+    }
+    public static void findGroupsByInterestingPeople(int[] ids, String[] names, ArrayList<People> people) throws IOException {
         ArrayList<People> interestingPeople = new ArrayList<People>();
 
         for(int i=0;i<ids.length;i++){
